933ad1a0a1a119c7ac51949f9c7da1422d0ab99e
Hans Dijkema <hans@dykema.nl>
2020-07-02 16:54:34 +0200

    modulemaker

---
+++ build-pollingsignals.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# vi: set sw=4 ts=4:
+
+BE='./build-ext.sh'
+if [ -e build-ext.sh ]; then
+    cp build-ext.sh build-ext.dist.sh
+    chmod 755 build-ext.dist.sh
+else
+    BE='./build-ext-dist.sh'
+fi
+
+CMD=$1;
+VERSION="0.1.1"
+EXT="polling-signals-extension"
+NAME="Polling Signals"
+DESCRIPTION="Implements a way to inform the front-end of changes (made by formulas) in the backend"
+MODULE=PollingSignals
+
+$BE "$CMD" "$VERSION" "$EXT" "$NAME" "$DESCRIPTION" "$MODULE"
+
